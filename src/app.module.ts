d1c0b113586bbd7983c92302961813e0dbf32d96
fredrichmonigue <muhammedbugracimen@gmail.com>
2026-03-08 00:55:43 +0300

    env-setup added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { envSchema } from './config/env.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: (env) => envSchema.parse(env),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/env.schema.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+export const envSchema = z.object({
+  PORT: z.coerce.number().int().positive().default(5000),
+  NODE_ENV: z.enum(['development', 'production', 'test']),
+  JWT_SECRET_KEY: z
+    .string()
+    .min(10, 'JWT_SECRET_KEY must be at least 10 characters long'),
+  DATABASE: z.string().min(1, 'Database url error'),
+});
+
+export type Env = z.infer<typeof envSchema>;
